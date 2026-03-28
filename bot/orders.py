ee44a825a8d42bbac8f7c304cb55395933e5a6c6
Annu9111 <aksoni911190@gmail.com>
2026-03-28 08:56:08 +0530

    orders

--- bot/orders.py
+++ bot/orders.py
@@ -4,3 +4,16 @@ from bot.client import BinanceClient
 class OrderService:
     def __init__(self):
         self.client = BinanceClient().get_client()
+        
+        def place_market_order(self, symbol, side, quantity):
+            try:
+                order = self.client.futures_create_order(
+                    symbol=symbol,
+                    side=side,
+                    type="MARKET",
+                    quantity=quantity
+                )
+                return order
+            except Exception as e:
+                print("Error placing market order:", str(e))
+                return None    
